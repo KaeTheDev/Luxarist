2685d6f36ae1aec5efa4f9becb0e5dd058f1b30f
KaeTheDev <kaethedev@gmail.com>
2026-02-05 23:28:51 -0500

    Initial project directory setup for client and server

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl text-blue-500">Luxarist Professional Project, Themes and Templates in motion!</h1>
+    </>
+  )
+}
+
+export default App
