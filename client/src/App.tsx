7b99d665f6573c8f4c35072917febbc7334f6e7c
KaeTheDev <kaethedev@gmail.com>
2026-02-21 00:08:13 -0500

    Added featured collections images for each category. Added featuredCategories dummy data to data.ts, created FeaturedCollectionsSection component with dummy data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import { Navbar } from "./common/layout/navigation/Navbar"
-import { HeroSection } from "./features/home/sections/HeroSection"
+import { Navbar } from "./common/layout/navigation/Navbar";
+import { FeaturedCollectionsSection } from "./features/home/sections/FeaturedCollectionsSection";
+import { HeroSection } from "./features/home/sections/HeroSection";
+
 function App() {
   return (
     <>
-    {/* <h1 className="text-3xl text-blue-500">Luxarist Professional Project, Themes and Templates in motion!</h1> */}
     <Navbar />
     <HeroSection />
+    <FeaturedCollectionsSection />
     </>
-  )
-}
+  );
+};
 
 export default App
