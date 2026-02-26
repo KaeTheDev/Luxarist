55603958295d5a67debcb901cc0124add0a864fc
KaeTheDev <kaethedev@gmail.com>
2026-02-26 16:17:24 -0500

    Moved ProductCard to common ui folder and created ProductList component

---
+++ client/src/common/ui/ProductList.tsx
@@ -0,0 +1,42 @@
+import { ProductCard } from "./ProductCard";
+import type { ProductPreview } from "../../types/ProductPreview";
+
+
+interface ProductListProps {
+    products: ProductPreview[];
+    emptyMessage?: string;
+    className?: string;
+}
+
+export function ProductList({ products, emptyMessage, className }: ProductListProps) {
+    // Handle Empty State
+    if(products.length === 0) {
+    return (
+        <div className="flex flex-col items-center justify-center py-24 px-4 text-center border border-dashed border-gray-200 rounded-xl">
+            <p className="text-gray-400 font-light italic tracking-wide">{emptyMessage}</p>
+            <button onClick={()=> window.location.reload()} className="mt-6 text-10px tracking-0 2em uppercase border-b border-black pb-1 hover:text-gray-500 hover:border-gray-500 transition-all">
+                Refresh Gallery
+            </button>
+        </div>
+    );
+}
+
+    // Render Grid
+    return (
+        <div className={`grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ${className}}`}>
+            {products.map((product) => (
+                <ProductCard 
+                key={product._id}
+                id={product._id}
+                imageUrl={product.primaryImageUrl}
+                title={product.name}
+                category={product.category.name}
+                price={product.price}
+                // Note: We don't pass a width like 'w-72' here. 
+                // The grid-cols will dictate the width automatically.
+                className="w-full"
+                />
+            ))}
+        </div>
+    );
+}
