78bc70d3a50bdd9233c6cf889669c51f8f97a394
KaeTheDev <kaethedev@gmail.com>
2026-03-19 01:06:00 -0400

    Created OrderCard component and added it to CustomerDashboard for testing

--- client/src/features/dashboard/CustomerDashboard/CustomerDashboard.tsx
+++ client/src/features/dashboard/CustomerDashboard/CustomerDashboard.tsx
@@ -1,7 +1,14 @@
+import OrderCard from "./components/OrderCard";
 import ProfileOverview from "./components/ProfileOverview";
 
 export function CustomerDashboard() {
     return(
+       <>
         <ProfileOverview />
+        <OrderCard orderNumber="1234" orderDate="3/19/26" total={20} status="Pending" />
+        <OrderCard orderNumber="1234" orderDate="3/19/26" total={20} status="Pending" />
+        <OrderCard orderNumber="1234" orderDate="3/19/26" total={20} status="Pending" />
+
+       </>
     )
 }
